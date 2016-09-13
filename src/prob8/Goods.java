ba50947c10aea93f55b03d2962364f3a6f2d2b38
yj <bit-user@bit>
2016-09-13 16:38:02 +0900

    전부 완료

--- src/prob8/Goods.java
+++ src/prob8/Goods.java
@@ -1,24 +1,32 @@
 package prob8;
 
-import java.util.Scanner;
-
 public class Goods {
-	public static void main(String[] args) {
-		// Goods[] goodsArray = new Goods[3];
-		Scanner sc = new Scanner(System.in);
+	private String name;
+	private String prices;
+	private String countStock;
 
-		for (int i = 0; i < 3; i++) {
-			String line = sc.nextLine();
-			String []infos = line.split(" ");
-			
-			String name = infos[0];
-			int prics = Integer.parseInt(infos[1]);
-			int countStock = Integer.parseInt(infos[2]);
-			
-			System.out.println(name);
-			System.out.println(prics);
-			System.out.println(countStock);
-		}
-		sc.close();
+	public String getName() {
+		return name;
 	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPrices() {
+		return prices;
+	}
+
+	public void setPrices(String prices) {
+		this.prices = prices;
+	}
+
+	public String getCountStock() {
+		return countStock;
+	}
+
+	public void setCountStock(String countStock) {
+		this.countStock = countStock;
+	}
+
 }
--- src/prob8/GoodsApp.java
+++ src/prob8/GoodsApp.java
@@ -1,8 +1,30 @@
 package prob8;
 
+import java.util.Scanner;
+
 public class GoodsApp {
 
 	public static void main(String[] args) {
-		
+		Goods[] goodsArray = new Goods[3];
+		Scanner sc = new Scanner(System.in);
+
+		for (int i = 0; i < 3; i++) {
+			String line = sc.nextLine();
+			String[] infos = line.split(" ");
+			
+			goodsArray[i] = new Goods();
+
+			goodsArray[i].setName(infos[0]);
+			goodsArray[i].setPrices(infos[1]);
+			goodsArray[i].setCountStock(infos[2]);
+
+		}
+		for (int i = 0; i < 3; i++) {
+			System.out.println(goodsArray[i].getName() + "(가격:" + 
+							   goodsArray[i].getPrices() + "원)이 " + 
+							   goodsArray[i].getCountStock() + "개 입고 되었습니다.");
+		}
+		sc.close();
+
 	}
 }
