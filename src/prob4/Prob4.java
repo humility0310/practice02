99b6e79fca9cfe74e1670bed8e13c4ed7a2e02f1
yj <bit-user@bit>
2016-09-13 15:07:24 +0900

    4번완

--- src/prob4/Prob4.java
+++ src/prob4/Prob4.java
@@ -1,8 +1,30 @@
 package prob4;
 
+import java.util.Scanner;
+
 public class Prob4 {
 	public static void main(String[] args) {
 		/* 구현코드 */
+		Scanner sc = new Scanner(System.in);
+		System.out.print("금액 : ");
+
+		int money = sc.nextInt();
+		int[] won = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
+		int[] sum = new int[10];
+		for (int i = 0; i < 10; i++) {
+			if (money >= won[i]) {
+
+				sum[i] = money / won[i];
+				money -= won[i] * sum[i];
+				System.out.println(won[i] + "원 : " + sum[i] + "개");
+			}
+			else{
+				sum[i]=0;
+				System.out.println(won[i] + "원 : " + sum[i] + "개");
+				
+			}
+		}
+
+		sc.close();
 	}
 }
-
