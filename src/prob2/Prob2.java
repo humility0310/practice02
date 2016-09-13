1de7885fca25ed8abeaefd6a2ce07ee9b880d931
yj <bit-user@bit>
2016-09-13 14:48:13 +0900

    2번완료

--- src/prob2/Prob2.java
+++ src/prob2/Prob2.java
@@ -5,13 +5,17 @@ import java.util.Scanner;
 public class Prob2 {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner( System.in );
-		System.out.print( "숫자를 입력하세요: " );
-		
-		int inputNumber = scanner.nextInt();
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("숫자를 입력하세요: ");
 
+		int inputNumber = scanner.nextInt();
 		/* 여기에 구현 코드를 작성 합니다. */
+		int sum = 0;
 		
+		for (int i = inputNumber; i > 0; i -= 2) {
+			sum += i;
+		}
+		System.out.println("결과값 : " + sum);
 		scanner.close();
 	}
 
